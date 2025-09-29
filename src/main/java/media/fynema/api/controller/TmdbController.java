5836b931d1ca4bdfb4d970e29a39591cf1583ee8
M1000fr <formentel.remi@gmail.com>
2025-09-29 21:30:43 +0200

    feat(security): Enable method-level security in SecurityConfig and restrict movie search access to ADMIN role

--- src/main/java/media/fynema/api/controller/TmdbController.java
+++ src/main/java/media/fynema/api/controller/TmdbController.java
@@ -3,6 +3,7 @@ package media.fynema.api.controller;
 import lombok.RequiredArgsConstructor;
 import media.fynema.api.services.TmdbService;
 import media.fynema.api.services.TmdbService.TMDBResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ public class TmdbController {
     private final TmdbService tmdbService;
 
     @GetMapping("/movie/search")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public TMDBResponse searchMovies(@RequestParam String query) {
         return tmdbService.searchMovie(query);
     }
